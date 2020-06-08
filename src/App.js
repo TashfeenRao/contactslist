975239c6a04c7b3e3d74abe2d7af10b11281763a
tashfeenrao <raotashfeen25@gmail.com>
2020-06-09 01:08:01 +0500

    composing with components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
+class ContactList extends React.Component {
+  render() {
+    const contacts = this.props.contacts;
+
+  return   <ol>
+  {contacts.map((person) => (
+    <li key={person.name}>{person.name}</li>
+  ))}
+</ol>
+}
+
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContactList contacts={[
+      {name: 'tashi'},
+      {name: 'mana'},
+      {name: 'usama'}
+      ]}/>
+      <ContactList contacts={[
+      {name: 'faizo'},
+      {name: 'yummy'},
+      {name: 'abc'}
+      ]}/>
     </div>
   );
 }
