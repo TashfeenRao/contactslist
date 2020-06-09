382290219dffa9a5721f8b52e2cee87d280db83d
tashfeenrao <raotashfeen25@gmail.com>
2020-06-10 04:08:25 +0500

    added state to app component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ContactList from './contactList';
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "@karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "@richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "@tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
- ];
-
-function App() {
-  return (
-    <div className="App">
-      <ContactList contacts={contacts}/>
-    </div>
-  );
-}
-
+ class App extends Component {
+   state = {
+     contacts:
+      [
+        {
+          "id": "karen",
+          "name": "Tashfeen",
+          "handle": "@karen_isgrigg",
+          "avatarURL": "http://localhost:5001/karen.jpg"
+        },
+        {
+          "id": "richard",
+          "name": "Richard Kalehoff",
+          "handle": "@richardkalehoff",
+          "avatarURL": "http://localhost:5001/richard.jpg"
+        },
+        {
+          "id": "tyler",
+          "name": "Tyler McGinnis",
+          "handle": "@tylermcginnis",
+          "avatarURL": "http://localhost:5001/tyler.jpg"
+        }
+      ]
+    }
+    render() {
+      return (
+        <div className="App">
+          <ContactList contacts={this.state.contacts}/>
+        </div>
+      );
+    }
+ }
 export default App;
