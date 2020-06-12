6b20aa4792bde2cd478ee5a5cb2e0b3bf23026bd
tashfeenrao <raotashfeen25@gmail.com>
2020-06-13 02:04:52 +0500

    search contacts by filter

--- src/contactList.js
+++ src/contactList.js
@@ -18,20 +18,24 @@ class ContactList extends Component {
     }
 
     render() {
+        const { query } = this.state;
+        const { contacts, removeButton } = this.props;
+        const showingContacts = query === '' ? contacts : contacts.filter((c) => (
+            c.name.toLowerCase().includes(query.toLowerCase())
+        ))
         return (
-            <div className='contact-list'>
-             {JSON.stringify(this.state)}
+            <div className='list-contacts'>
                 <div className='list-contacts-top'>
                     <input
                         className='search-contacts'
                         type='text'
                         placeholder='Search Contacts'
-                        value={this.state.query}
+                        value={query}
                         onChange={(event) => this.updateQuery(event.target.value)}
                     />
                 </div>
                 <ol className='contact-list'>
-                    {this.props.contacts.map((person) => (
+                    {showingContacts.map((person) => (
                         <li key={person.id} className='contact-list-item'>
                             <div className='contact-avatar' style={{ backgroundImage: `url(${person.avatarURL})` }}>
                             </div>
@@ -39,7 +43,7 @@ class ContactList extends Component {
                                 <p>{person.name}</p>
                                 <p>{person.handle}</p>
                             </div>
-                            <button className='contact-remove' onClick={() => this.props.removeButton(person)}>Remove</button>
+                            <button className='contact-remove' onClick={() => removeButton(person)}>Remove</button>
                         </li>
                     ))}
                 </ol>
