e65148e73ee24e2c50c5824a706c2c99a529d100
tashfeenrao <raotashfeen25@gmail.com>
2020-06-16 01:31:12 +0500

    renderd form for adding contacts

--- src/createContact.js
+++ src/createContact.js
@@ -3,9 +3,22 @@ import { Link } from 'react-router-dom';
 class CreateContact extends Component {
     render() {
         return(
+            <div>
             <Link className='close-create-contact' to='/'>
                 Close
             </Link>
+            <form className='create-contact-form'>
+                <imageInput 
+                className='create-contact-avatar-input'
+                name='avatarURL'
+                maxHeigth={64} />
+                <div className='create-contact-details'>
+                    <input type='text' name='name' placeholder='Name' />
+                    <input type='text' name='handle' placeholder='Handle' />
+                    <button>Add contact</button>
+                </div>
+            </form>
+            </div>
         )
     }
 }
