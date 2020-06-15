a57d1651019030fc03c6e1fd718f4ecaefcd6063
tashfeenrao <raotashfeen25@gmail.com>
2020-06-15 23:02:07 +0500

    screen state property added for UI switching

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ContactList from './contactList';
 import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './createContact';
  class App extends Component {
    state = {
      contacts:
-      []
+      [],
+      screen: 'list'
     }
     componentDidMount() {
       ContactsAPI.getAll().then((contacts) => {
@@ -24,7 +26,11 @@ import * as ContactsAPI from './utils/ContactsAPI'
     render() {
       return (
         <div className="App">
-          <ContactList contacts={this.state.contacts} removeButton={this.removeContact}/>
+          {this.state.screen === 'list' && 
+              <ContactList contacts={this.state.contacts} removeButton={this.removeContact}/>
+          }
+          {this.state.screen === 'create' && <CreateContact />}
+          
         </div>
       );
     }
---
+++ src/createContact.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CreateContact extends Component {
+    render() {
+        return(
+            <div>
+                Creaate Contact
+            </div>
+        )
+    }
+}
+export default CreateContact
