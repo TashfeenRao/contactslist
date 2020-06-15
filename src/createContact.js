23108f3851198b2a41afc5288e1dc4460d22ab90
tashfeenrao <raotashfeen25@gmail.com>
2020-06-16 01:22:33 +0500

    create contact back icon added

--- src/createContact.js
+++ src/createContact.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 class CreateContact extends Component {
     render() {
         return(
-            <div>
-                Creaate Contact
-            </div>
+            <Link className='close-create-contact' to='/'>
+                Close
+            </Link>
         )
     }
 }
